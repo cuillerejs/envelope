36f7a7e6878a7d13270ef581561074d6628162f5
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-10-19 22:58:17 +0200

    :alembic: Testing bin setting

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
+#!/usr/bin/env ts-node-script
+
 import cuillere from '@cuillere/core'
 import { channelsPlugin } from '@cuillere/channels'
 
 import { listenAndServe } from './envelope'
 
-cuillere(channelsPlugin()).call(listenAndServe, 'localhost', 8080).catch((e) => {
+const host = process.env.HOST ?? 'localhost'
+const port = Number(process.env.PORT ?? 8080)
+
+cuillere(channelsPlugin()).call(listenAndServe, host, port).catch((e) => {
   console.error(e)
   process.exit(1)
 })
